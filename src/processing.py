293c6bbc41b5f1245aad526f26e7c51a880acee7
Elizaveta <you@example.com>
2024-11-25 00:20:56 +0300

    add

--- src/processing.py
+++ src/processing.py
@@ -16,7 +16,7 @@ def filter_by_state(word, state="EXECUTED"):
 print(filter_by_state(words))
 
 
-dates = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+by_date = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
@@ -27,4 +27,4 @@ def sort_by_date(date, reverse: bool = True):
     return sorted(date, key=lambda k: '.'.join(reversed(k['date'].split('.'))), reverse=reverse)
 
 
-print(sort_by_date(dates))
+print(sort_by_date(by_date))
