858a0fc4a936874fdbc37e6d724c541785c6d98a
Elizaveta <you@example.com>
2024-12-03 14:17:41 +0300

    init tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -6,7 +6,7 @@ from src.masks import get_mask_account, get_mask_card_number
 @pytest.fixture
 def masks():
     """Функция возвращения вывода"""
-    return["700079 ** **** 6361", "** 4305"]
+    return ["700079 ** **** 6361", "** 4305"]
 
 
 def test_get_mask_card_number():
--- tests/test_procerssing.py
+++ tests/test_procerssing.py
@@ -6,20 +6,22 @@ from src.processing import filter_by_state, sort_by_date
 @pytest.fixture
 def processing():
     """Функция возвращения вывода"""
-    return["{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}",
-           "{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}",
-           "{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}",
-           "{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}"]
+    return ["{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}",
+            "{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}",
+            "{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}",
+            "{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}"]
 
 
-def test_filter_by_state():
+@pytest.mark.parametrize("input_data, state, expected", [
+    ([{'state': 'active'}, {'state': 'inactive'}], 'active', [{'state': 'active'}]),
+    ([{'state': 'active'}, {'state': 'inactive'}], 'inactive', [{'state': 'inactive'}]),
+
+])
+def test_filter_by_state(input_data, state, expected):
     """Функция проверки фильтра"""
-    assert filter_by_state != [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                               {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+    assert filter_by_state(input_data, state) == expected
 
 
-@pytest.mark.parametrize("dat_account", [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
-def test_sort_by_date(dat_account):
-    """Функция проверки длинны"""
-    assert len(dat_account) > 0
+def test_sort_by_date():
+    """Функция проверки не пустого словаря"""
+    assert sort_by_date != []
