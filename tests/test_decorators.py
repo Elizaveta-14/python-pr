7b192561ac171d32c5cb0671f1c1223bdb6e4204
Elizaveta <you@example.com>
2024-12-13 18:01:05 +0300

    decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,34 @@
+import pytest
+
+from src.decorators import log
+
+
+@log(filename="mylog.txt")
+def my_function(x: int | str, y: int | str) -> int:
+    """Функция суммирует два числа и возвращает результат"""
+    try:
+        return int(x) + int(y)
+    except ValueError as e:
+        print("Ошибка ввода! Пожалуйста, вводите только целые числа.")
+        raise e
+
+
+def test_log_save_file() -> None:
+    positive_result = my_function(2, 2)
+    assert positive_result == 4
+
+
+def test_log_captured(capsys) -> None:
+    @log(filename=None)
+    def my_function(x: int, y: int) -> int:
+        """Функция суммирует два числа и возвращает результат"""
+        return x + y
+
+    my_function(2, 2)
+    captured = capsys.readouterr()
+    assert captured.out == "my_function ok\n"
+
+
+def test_log_decorator():
+    with pytest.raises(Exception, match="a"):
+        my_function()
