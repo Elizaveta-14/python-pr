4c59b9e3550b1fbc579bfd8b891cf61905730339
Elizaveta <you@example.com>
2024-12-27 00:06:35 +0300

    test

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,36 +1,36 @@
-# from unittest.mock import patch
-#
-# from src.external_api import get_convert_amount, process_transaction
-#
-#
-# @patch("json.loads")
-# def test_get_convert_amount(mock_get):
-#     """Проверяет статус коде 200"""
-#     mock_get.return_value.status_code = 200
-#     assert get_convert_amount("https://www.cbr-xml-daily.ru//daily_json.js") != "Failed to get currency rate"
-#
-#
-# @patch("json.loads")
-# def test_convert_transaction_to_rub(mock_get):
-#     """Проверяет на возвращение словаря"""
-#     mock_get.return_value.json.return_value = {
-#         "date": "2018-02-22",
-#         "historical": "",
-#         "info": {"rate": 148.972231, "timestamp": 1519328414},
-#         "query": {"amount": 1, "from": "USD", "to": "RUB"},
-#         "result": 104.461,
-#         "success": "true",
-#     }
+import unittest
+from unittest.mock import patch
+import json
+import os
+import requests
+from src.external_api import get_convert_amount, convert_transaction_to_rub
+import pytest
 
-#
-# @patch("json.loads")
-# def test_convert_amount(mock_get):
-#     """Проверяет на валюту"""
-#     mock_get.return_value.currency_data = "Valute"
-#     assert get_convert_amount("https://www.cbr-xml-daily.ru//daily_json.js") != "No data for currency"
-#
-#
-# @patch("json.loads")
-# def test_process_transaction(transaction):
-#     assert process_transaction(transaction) != []
-#
+
+
+@patch("json.loads")
+def test_get_convert_amount(mock_get):
+    """Проверяет статус коде 200"""
+    mock_get.return_value.status_code = 200
+    assert get_convert_amount != []
+
+
+@patch("json.loads")
+def test_convert_transaction_to_rub(mock_get):
+    """Проверяет на возвращение словаря"""
+    mock_get.return_value.json.return_value = {
+                 "date": "2018-02-22",
+                 "historical": "",
+                 "info": {"rate": 148.972231, "timestamp": 1519328414},
+                "query": {"amount": 1, "from": "USD", "to": "RUB"},
+                "result": 104.461,
+                 "success": "true",
+            }
+
+
+@patch("builtins.open", side_effect=KeyError)
+def test_file_not_found(amount):
+    """Проверяет на возвращение пустого списка при ошибке"""
+    transactions = get_convert_amount(amount)
+    assert transactions == 0
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,21 @@
-from unittest.mock import patch
-
+import pytest
+from unittest.mock import patch, MagicMock
+from pathlib import Path
 from src.utils import get_transactions
 
 
 @patch("json.loads")
 def test_get_transactions(mock_get):
     """Проверяет на возвращение списка"""
-    mock_get.return_value = []
-    assert get_transactions(mock_get) == []
-   
+    mock_get.return_value = ""
+    assert get_transactions(mock_get) == ""
+
+
+@patch("builtins.open", side_effect=FileNotFoundError)
+def test_file_no_found(mock_file):
+    """Проверяет на возвращение пустого списка при ошибке"""
+    transactions = get_transactions("data/operations.json")
+    assert transactions == []
+
+
+
